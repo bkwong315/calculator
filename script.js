3cee052b5502aa6018d879a680a4ea6091cbd766
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2022-12-22 16:46:20 -0800

    Display updates with numbers and operations

--- script.js
+++ script.js
@@ -5,3 +5,37 @@ const sub = (a, b) => a - b;
 const mult = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+const operate = (num1, num2, operation) => {
+  let result = 0;
+
+  switch (operation) {
+    case "+":
+      result = add(num1, num2);
+      break;
+    case "-":
+      result = sub(num1, num2);
+      break;
+    case "*":
+      result = mult(num1, num2);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      break;
+    default:
+      console.log(`Operation ${operation} is not supported.`);
+  }
+
+  return result;
+};
+
+window.addEventListener("load", (e) => {
+  const displayBtns = document.querySelectorAll(".btn:not(.no-display)");
+  displayBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const display = document.querySelector(".display");
+      display.textContent += e.target.getAttribute("value");
+    });
+  });
+  console.log(displayBtns);
+});
