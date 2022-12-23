29ce7907ac9fcff0a92edb3922e3502b276dd0a6
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2022-12-22 18:04:28 -0800

    Add equal button logic for arithmetic

--- script.js
+++ script.js
@@ -29,13 +29,68 @@ const operate = (num1, num2, operation) => {
   return result;
 };
 
+const evaluate = () => {
+  const display = document.querySelector(".display");
+
+  let equation = display.textContent;
+  let parsedEquation = equation.split(/([+\-*/])/g);
+
+  while (parsedEquation.length > 1) {
+    let prevNum;
+    let prevNumIdx;
+    let nextNum;
+    let operation;
+
+    if (
+      parsedEquation.filter((element) => (element === "*") | (element === "/"))
+        .length > 0
+    ) {
+      for (let i = 0; i < parsedEquation.length; i++) {
+        if (parsedEquation[i] === "*" || parsedEquation[i] === "/") {
+          operation = parsedEquation[i];
+          prevNumIdx = i - 1;
+          prevNum = parseInt(parsedEquation[i - 1]);
+          nextNum = parseInt(parsedEquation[i + 1]);
+        }
+      }
+      parsedEquation.splice(
+        prevNumIdx,
+        3,
+        operate(prevNum, nextNum, operation)
+      );
+    } else if (
+      parsedEquation.filter((element) => (element === "+") | (element === "-"))
+        .length > 0
+    ) {
+      for (let i = 0; i < parsedEquation.length; i++) {
+        if (parsedEquation[i] === "+" || parsedEquation[i] === "-") {
+          operation = parsedEquation[i];
+          prevNumIdx = i - 1;
+          prevNum = parseInt(parsedEquation[i - 1]);
+          nextNum = parseInt(parsedEquation[i + 1]);
+        }
+      }
+      parsedEquation.splice(
+        prevNumIdx,
+        3,
+        operate(prevNum, nextNum, operation)
+      );
+    }
+  }
+
+  console.log(parsedEquation);
+};
+
 window.addEventListener("load", (e) => {
   const displayBtns = document.querySelectorAll(".btn:not(.no-display)");
+  const equalBtn = document.querySelector(".equal-btn");
+
   displayBtns.forEach((btn) => {
     btn.addEventListener("click", (e) => {
       const display = document.querySelector(".display");
       display.textContent += e.target.getAttribute("value");
     });
   });
-  console.log(displayBtns);
+
+  equalBtn.addEventListener("click", evaluate);
 });
